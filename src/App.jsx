5094797aacbf571080f4f85e4316046751f1e144
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-11-19 07:13:43 +0330

    added react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Header.jsx";
+
+function App() {
+  return (
+    <div className="container-fluid p-">
+      <Header />
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <div className="row">
+        <div className="col-12 col-md-12 col-lg-12">
+          <div className="header-box w-100"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
